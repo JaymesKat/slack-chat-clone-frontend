d2a47e7a6178cf23913d8cf0338ca7f3597f4648
James <jpkatarikawe@gmail.com>
2020-08-29 21:59:34 +0300

    Update API gateway ID

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
-const apiId = 'jh1yx4w8z9'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = "owj92eb2sj";
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`;
 
 export const authConfig = {
-  domain: 'reactauth0-test.auth0.com',    // Name of the Auth0 domain
-  clientId: 'rTDVBgu27BIhIBGLzubVyqg2wHC22Cos',  // Client id of a new application
-  callbackUrl: process.env.REACT_APP_CALLBACK_URL
-}
+  domain: "reactauth0-test.auth0.com", // Name of the Auth0 domain
+  clientId: "rTDVBgu27BIhIBGLzubVyqg2wHC22Cos", // Client id of a new application
+  callbackUrl: process.env.REACT_APP_CALLBACK_URL,
+};
